e235a2de80193bdd76d6eb93e91388b205e12d67
Default Profile User <pbhanda1@bl-n10.pods.bu.int>
2017-01-20 15:33:48 -0500

    added long press gesture recognizer

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -23,20 +23,28 @@ class ViewController: UIViewController,MKMapViewDelegate {
     func setMap()
     {
         var lat:CLLocationDegrees = 43.095
-        
         var long:CLLocationDegrees = -79.006
-        
         var latDelta:CLLocationDegrees = 0.05
-        
         var longDelta:CLLocationDegrees = 0.05
-        
         var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
-        
         var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
-        
         var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        
         map.setRegion(region, animated: false)
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "My Location"
+        annotation.subtitle = "My dream place"
+        
+        map.addAnnotation(annotation)
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+    }
+    
+    func action(gestureRecognizer : UIGestureRecognizer) {
+        print("Recognized")
     }
 
     override func didReceiveMemoryWarning() {
